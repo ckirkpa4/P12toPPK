ff80fbf67a8a3d5deb5fc776c0c2809a1ac44473
ckirkpa4 <ckirkpa4@gmail.com>
2023-09-01 11:12:10 -0400

    first commit

---
+++ P122PPK.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+int main()
+{
+    std::string p12;
+    std::string ppk;
+    std::string password;
+
+    std::cout << "Enter the .p12 directory: ";
+    std::cin >> p12;
+    std::cout << "Enter the .ppk output directory: ";
+    std::cin >> ppk;
+    std::cout << "Enter your .p12 password: ";
+}
