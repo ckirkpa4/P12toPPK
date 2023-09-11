64db4ac67e6c2e39e2a45e3a39e711194fec7ce9
Chris Kirk <ckirkpa4@gmail.com>
2023-09-10 20:49:54 -0400

    extracting pem

--- P122PPK.cpp
+++ P122PPK.cpp
@@ -5,12 +5,20 @@
 int main()
 {
     std::string p12;
+    std::string pem;
     std::string ppk;
     std::string password;
 
     std::cout << "Enter the .p12 directory: ";
     std::cin >> p12;
+    std::cout << "Enter the .pem output directory: ";
+    std::cin >> pem;
     std::cout << "Enter the .ppk output directory: ";
     std::cin >> ppk;
     std::cout << "Enter your .p12 password: ";
+    std::cin >> password;
+
+    std::string extractPem = "openssl pkcs12 -in \"" + p12 + "\" -clcerts -nokeys -out \"" + pem + "\" -passin pass:" + password;
+
+    int extractResult=std::system(extractPem.c_str());
 }
